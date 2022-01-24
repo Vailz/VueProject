3f491069f6721b00952510b6e31cecdc13f360d8
Vailz <shevchenko.d.p.i20@gmail.com>
2022-01-24 21:42:49 +0300

    br_20

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,44 +1,42 @@
 <template> 
-	<v-container>
-		<v-layout row> 
-			<v-flex xs12>
-				<v-card class="mt-5" v-if="!loading">
-							<v-img 
-							height="300px"
-							:src="ad.src">
-							</v-img>
-							<v-card-text>
-								<h1 class="text--primary mb-5"> {{ ad.title }} </h1>
-								<p> {{ ad.desc }} </p>
-							</v-card-text>
-							<v-card-actions>
-								<v-spacer></v-spacer>
-								<modal-dialog :ad="ad">
-								</modal-dialog>
-								<v-btn text class="success">Buy</v-btn>
-							</v-card-actions>
-				</v-card>
-				<div v-else>
-					<v-container>
-						<v-layout row>
-							<v-flex xs12 sm2 offset-sm6 class="mt-5">
-								<v-progress-circular 
-								:size="70"
-								:width="7"
-								color="primary"
-								indeterminate
-								></v-progress-circular>
-							</v-flex>
-						</v-layout>
-					</v-container>
-				</div>
-			</v-flex>
-		</v-layout>
-	</v-container>
+    <v-container>
+        <v-layout row> 
+            <v-flex xs12>
+                <v-card class="mt-5" v-if="!loading">
+                   <v-img 
+                     height="300px" :src="ad.src">
+                   </v-img>
+                  <v-card-text>
+                      <h1 class="text--purple mb-3">{{ad.title}}</h1>
+                      <p>{{ad.desc}}</p>
+                  </v-card-text>
+                  <v-card-actions>
+                     <v-spacer></v-spacer>
+                     <modal-dialog :ad= "ad" v-if="isOwner"></modal-dialog>
+                     <v-btn class="success">Buy</v-btn>  
+                  </v-card-actions>	
+                </v-card>
+                <div v-else>
+                    <v-container>
+                        <v-layout row>
+                            <v-flex xs12 sm12 offset-sm6 class="mt-5">
+                                <v-progress-circular
+                                 :size="70"
+                                 :width="7"
+                                 color="#800080"
+                                 indeterminate>
+                                </v-progress-circular>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </div>
+            </v-flex>
+        </v-layout> 
+    </v-container>
 </template>
 
 <script>
-import EditAdModal from './EditAdModal'
+import EditAdModal from './EditAdModal.vue'
 export default {
     props:['id'],
     computed:{
@@ -46,12 +44,15 @@ export default {
             const id=this.id
             return this.$store.getters.adById(id)
         },
-		loading(){
-			return this.$store.getters.loading
-		}
+        loading(){
+            return this.$store.getters.loading
+        },
+        isOwner(){
+            return this.ad.ownerId === this.$store.getters.user.id
+        }
     },
-	components: {
-		'modal-dialog': EditAdModal
-	}
+    components:{
+        'modal-dialog':EditAdModal
+    }
 } 
 </script>
