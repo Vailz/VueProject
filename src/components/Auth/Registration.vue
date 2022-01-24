2999d35cf396bff0d83ee9f8bb8fe36de51cf49c
Vailz <shevchenko.d.p.i20@gmail.com>
2022-01-24 11:21:55 +0300

    br_14.1_14.4

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -41,7 +41,8 @@
 					<v-btn 
 					color="primary"
 					@click="onSubmit"
-					:disabled="!valid"
+					:loading="loading"
+					:disabled="!valid || loading"
 					>Create Account</v-btn>
 				</v-card-actions>
 			</v-card>
@@ -72,14 +73,27 @@ export default {
 			]
 		} 	
 	},
+	computed: {
+		loading() {
+			return this.$store.getters.loading
+		}
+	},
 	methods: {
 		onSubmit(){
+			if (this.$refs.form.validate()){
 			const user = {
 				email: this.email,
 				password: this.password
 			}
 			this.$store.dispatch('registerUser', user)
+			.then(() => {
+				this.$router.push("/")
+			})
+			.catch((err) => {
+			console.log(err.message)
+			})
 		}
 	}
 } 
+}
 </script>
